5d29b3c9e6ad8eec3b81ea4c074e920326b31006
1canhhoa <nhamvanhien31102002@gmail.com>
2023-04-14 07:42:08 +0700

    all

---
+++ src/server.js
@@ -0,0 +1,24 @@
+21 lines (16 sloc)  506 Bytes
+ 
+
+// const express = require('express')
+import express from 'express'
+import configViewEngine from './configs/ViewEngine'
+const app = express()
+
+require('dotenv').config() // muốn lấy port từ .env bắt buộc phải cài dotenv và require dotenv
+const port = process.env.PORT || 8080
+
+configViewEngine(app)
+
+app.get('/',(req,res) => {
+  res.render('test/index.ejs')
+})
+
+app.get('/', (req, res) => {
+  res.send('Nham Van Hien')
+})
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`)
+})
